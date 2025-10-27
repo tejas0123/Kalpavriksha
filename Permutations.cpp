38d69698d283a2807f0c849de46a0e41057cd854
Tejas R <94542899+tejas0123@users.noreply.github.com>
2025-10-27 17:26:59 +0530

    Updated Permutations.cpp

--- Permutations.cpp
+++ Permutations.cpp
@@ -1,7 +1,20 @@
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
 
 int main(){
   int n;
   cin>>n;
+
+  if(n <= 4){
+    cout<<"NO SOLUTION";
+  } else{
+    int i;
+    int arr[n];
+
+    for(i = 0; i < n; i++){
+      arr[i] = i + 1;
+    }
+
+   
+  }
 }
